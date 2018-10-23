9b255503e40f454e59cc5963943af7dc528e1edf
Mauricio Vieira <mauriciomvdr@hotmail.com>
2018-10-23 14:25:23 -0300

    Medidas de tempo atualizadas, removida a opção de escrever no arquivo

--- src/Principal/UDPClient.java
+++ src/Principal/UDPClient.java
@@ -5,18 +5,13 @@
  */
 package Principal;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,37 +23,7 @@ import java.util.logging.Logger;
 public class UDPClient {
     
     private static final int MAX_TIMEOUT = 250;
-    private static final int QTD_PACOTES_ENVIADOS = 10;
-    private Calendar cal;
-    private SimpleDateFormat sdf;
-    
-    public void regLog(List<String> l) {
-        BufferedWriter bw = null;
-        try {
-            bw = new BufferedWriter(new FileWriter("log.txt", true));
-            cal = Calendar.getInstance();
-            sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:SS");
-            bw.write(sdf.format(cal.getTime()));
-            bw.newLine();
-            for(String s : l) {
-                bw.write(s);
-                bw.newLine();
-            }
-            bw.newLine();
-            
-        } catch (IOException e) {
-            System.out.println("Erro ao abrir o arquivo de log");
-        } finally {    
-            if (bw != null) {
-                try {
-                    bw.close();
-                } catch (IOException ex) {
-                    System.out.println("Erro ao fechar o arquivo de log");
-                }
-            }
-        
-        }
-    }
+    private static final int QTD_PACOTES_ENVIADOS = 20;
     
     public static void main(String[] args) throws SocketException, UnknownHostException, IOException {
         
@@ -85,24 +50,25 @@ public class UDPClient {
         while (pack <= QTD_PACOTES_ENVIADOS) {
                 
             String sentence = "pacote " + pack;
-            //System.out.println("Enviando " + sentence);
+
             sendData = sentence.getBytes();
 
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, porta);
 
             System.out.println("Enviando " + sentence + " UDP para " + servidor + ":" + porta);
+            
+            start = System.nanoTime();
 
-            //start = System.currentTimeMillis();
-            start = System.currentTimeMillis();
-            //System.out.println("start " + start);
             clientSocket.send(sendPacket);
             
             try {
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                                 
                 clientSocket.receive(receivePacket);
-                end = System.currentTimeMillis();                
+                end = System.nanoTime();
+                
                 rttReceive = end - start;
+                System.out.println(rttReceive);
                 
                 if (rttReceive < min) min = rttReceive;
                 if (rttReceive > max) max = rttReceive;
@@ -117,11 +83,11 @@ public class UDPClient {
                 
                 qtdPacksReceive++;
                 
-                Thread.sleep(1000 - rttReceive);
+                Thread.sleep(1000000000 - rttReceive);
 
                 //clientSocket.close();
             } catch (IOException | InterruptedException e) {
-                    end = System.currentTimeMillis();
+                    end = System.nanoTime();
                     long tempoAux = end - start;
                     System.out.println("Error, tempo gasto foi de "+ (end-start));
                 try {
@@ -135,38 +101,23 @@ public class UDPClient {
             pack++;
             
         }
+        double min2 = min/1000000;
+        double max2 = max/1000000;
+        avg = (totalTime/qtdPacksReceive)/1000000;
         clientSocket.close();
         System.out.println("Socket cliente fechado!");
         float porcentagemPerda = 100 - ((100*qtdPacksReceive)/QTD_PACOTES_ENVIADOS);
         int lostPacks = QTD_PACOTES_ENVIADOS - qtdPacksReceive;
-        avg = totalTime/rttReceive;
-        List<String> linhas = new ArrayList<String>();
-        String linha = "------------------------------------------------------------------------------------";
-        System.out.println(linha);
-        linhas.add(linha);
-        linha = "|Estatistica do ping para: " + IPAddress + " na porta " + porta + "           |";
-        System.out.println(linha);
-        linhas.add(linha);
-        linha = "|Pacotes: Enviados = " + QTD_PACOTES_ENVIADOS + ", Recebidos = " + qtdPacksReceive + ", Perdidos = " + lostPacks + " ("+ porcentagemPerda + "% de perda)              |";
-        System.out.println(linha);
-        linhas.add(linha);
-        linha ="|Tempo aproximado em milissegundos: Mínimo = " + min + " ms, Máximo = " + max + " ms, Média = " + avg + " ms  |";
-        System.out.println(linha);
-        linhas.add(linha);
-        linha = "------------------------------------------------------------------------------------";
-        System.out.println(linha);
-        linhas.add(linha);
-        
-        //escrevendo no arquivo de logs
-        new UDPClient().regLog(linhas);
-        //System.out.println("------------------------------------------------------------------------------------");
-        //System.out.println("|Estatistica do ping para: " + IPAddress + " na porta " + porta + "           |");
-        //System.out.println("|Pacotes: Enviados = " + QTD_PACOTES_ENVIADOS + ", Recebidos = " + 
-        //        qtdPacksReceive + ", Perdidos = " + lostPacks + " ("+ porcentagemPerda + "% de perda)              |");
-        //System.out.println("|Tempo aproximado em milissegundos: Mínimo = " + min + " ms, Máximo = " + max + " ms, Média = " + 
-        //        avg + " ms  |");
-        //System.out.println("------------------------------------------------------------------------------------");
-        
+        //avg = totalTime/qtdPacksReceive;
+        StringBuilder resultado = new StringBuilder();
+        resultado.append("------------------------------------------------------------------------------------\n");
+        resultado.append("Estatistica do ping para: " + IPAddress + " na porta " + porta + "           \n");
+        resultado.append("Pacotes: Enviados = " + QTD_PACOTES_ENVIADOS + ", Recebidos = " + qtdPacksReceive + ", Perdidos = " + 
+                lostPacks + " ("+ porcentagemPerda + "% de perda)              \n");
+        resultado.append("Tempo aproximado em milissegundos: Mínimo = " + min + " ms, Máximo = " + max + " ms, Média = " + 
+                avg + " ms  \n");
+        resultado.append("------------------------------------------------------------------------------------\n");
+        System.out.println(resultado);
     }
         
 }
